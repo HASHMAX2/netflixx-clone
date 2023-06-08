30bb0266935a8db26dd59a25b2c77410d1289a2a
ali hashmi <ali.hashmi0@gmail.com>
2023-06-08 21:01:39 +0530

    Banner complete, adding Nav bar

--- src/Banner.js
+++ src/Banner.js
@@ -4,42 +4,65 @@ import requests from "./requests";
 import "./Banner.css";
 
 function Banner() {
+  let url;
   const [movie, setMovie] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
-      console.log(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length - 1)
-        ]
-      );
+      // console.log(request.data.results);
+
       setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
+
+      console.log(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+
       return request;
     }
     fetchData();
   }, []);
+
+  function truncateString(str, num) {
+    if (str && str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  }
   return (
-    <div
+    <header
       className="banner"
       style={{
         backgroundSize: "cover",
         backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`,
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
       }}
     >
       <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {/* {movie?.overview} */}
+          {movie?.overview !== null
+            ? truncateString(movie?.overview, 150)
+            : movie?.overview}
+        </h1>
       </div>
-      <div className="banner_Buttons">
-        <button className="banner_button">Play</button>
-        <button className="banner_button">My List</button>
-      </div>
-      <h1 className="banner_description">{movie?.overview}</h1>
-      {/* background img */}
-    </div>
+      <div className="fade"></div>
+    </header>
   );
 }
 
